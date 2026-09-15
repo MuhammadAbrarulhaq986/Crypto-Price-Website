b87fa6d6532ec2a9130b545c8c717d6b41b36042
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-09-15 17:08:56 +0500

    Useing coingecko api in this projects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { fetchCryptos } from "../api/coinGecko.js";
+
 export const Home = () => {
+  useEffect(() => {
+    fetchCryptoData();
+  }, []);
+  const fetchCryptoData = async () => {
+    const data = await fetchCryptos();
+    console.log(data);
+  };
+
   return (
     <>
       <div>This is the home page</div>
