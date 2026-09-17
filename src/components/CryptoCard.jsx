018a6e14f1e3ed16fc9a2be9a3d8a0e2aa4f6910
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-09-17 12:59:46 +0500

    Added a formatter.js file so everyone can understand the numbers

--- src/components/CryptoCard.jsx
+++ src/components/CryptoCard.jsx
@@ -1,3 +1,5 @@
+import { formatMarketCap, formatPrice } from "../utils/formatter.js";
+
 export const CryptoCard = ({ crypto }) => {
   return (
     <>
@@ -13,7 +15,28 @@ export const CryptoCard = ({ crypto }) => {
           </div>
         </div>
         <div className="crypto-price">
-          <p className="price">{crypto.current_price}</p>
+          <p className="price">{formatPrice(crypto.current_price)}</p>
+          <p
+            className={`change ${crypto.price_change_percentage_24h >= 0 ? "positive" : "negative"}`}
+          >
+            {crypto.price_change_percentage_24h >= 0 ? "⇡" : "⇣"}{" "}
+            {Math.abs(crypto.price_change_percentage_24h).toFixed(2)}%
+          </p>
+        </div>
+        <div className="crypto-stats">
+          <div className="stat">
+            <span className="stat-label">Market Cap </span>
+            <span className="stat-value">
+              ${formatMarketCap(crypto.market_cap)}
+            </span>
+          </div>
+
+          <div className="stat">
+            <span className="stat-label">Market Volume </span>
+            <span className="stat-value">
+              ${formatMarketCap(crypto.total_volume)}
+            </span>
+          </div>
         </div>
       </div>
     </>
